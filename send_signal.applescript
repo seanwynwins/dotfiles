25cb5debc9091b1447cdd9ea47a42037d5e3f396
seanwynwins <seanwynng@gmail.com>
2023-12-09 04:18:11 -0800

    add comments

--- send_signal.applescript
+++ send_signal.applescript
@@ -1,2 +1,3 @@
+# part 1/2 of a workflow where i send myself a message containing any highlighted text using signal-cli
 tell application "System Events" to keystroke "c" using {command down}
 delay 0.5
--- send_signal.sh
+++ send_signal.sh
@@ -1,3 +1,7 @@
+# part 2/2 of an apple automator workflow where i send myself messages containing highlighted text using signal-cli. perfect for 
+# saving anything interesting that you read!
+# i map the apple automator workflow to a key binding (can be done through keyboard shortcuts)
+# signal is an open source e2e messaging app where i save interesting tidbits of info
 #!/bin/zsh
 
 export PATH=$HOME/signal-cli/build/install/signal-cli/bin:$PATH
