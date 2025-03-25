69fd497a6a258d98870f6843495c3cd1717d4dec
Sean-Wyn Ng <seanwynng@proton.me>
2025-03-25 09:16:43 -0700

    need sleep for wayland to initialize

--- start_swww.sh
+++ start_swww.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 export PATH="$HOME/swww/target/release/:$PATH"
 
+sleep 2
+
+echo "Starting swww_daemon.sh" >> /tmp/swww.log
+~/swww_daemon.sh >> /tmp/swww.log 2>&1
+
 echo "Starting swww_schedule.sh" >> /tmp/swww.log
 ~/swww.sh >> /tmp/swww.log 2>&1
 
 echo "Starting swww_schedule.sh" >> /tmp/swww.log
 ~/swww_schedule.sh >> /tmp/swww.log 2>&1
 
-echo "Starting swww_daemon.sh" >> /tmp/swww.log
-~/swww_daemon.sh >> /tmp/swww.log 2>&1
